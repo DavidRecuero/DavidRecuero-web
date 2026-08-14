450448915bba5aa91e40e050038cb6156b147816
David Recuero <recuero.david@gmail.com>
2026-08-14 18:49:20 +0200

    feat(contact): contact form working

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -8,13 +8,12 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmailAction(data: EmailData) {
   try {
-    // MAIL WE RECEIVE
+    // Mail to receive from the contact form
     const { error } = await resend.emails.send({
-      // Resend API requires the 'from' field to be a verified sender email address
-      from: 'Portfolio Contact <onboarding@resend.dev>', 
+      from: 'Portfolio Web Contact <contact@davidrecuero.com>',
       to: process.env.CONTACT_EMAIL as string,
       replyTo: data.email, 
-      subject: `Portfolio: ${data.subject || 'New Message from ' + data.name}`,
+      subject: `Portfolio Web Contact: ${data.subject || 'New Message from ' + data.name}`,
       text: `Name: ${data.name}\nEmail: ${data.email}\n\nMessage:\n${data.message}`,
     });
 
@@ -22,22 +21,19 @@ export async function sendEmailAction(data: EmailData) {
       return { success: false, error: error.message };
     }
 
-    //WAITING FOR FINAL DOMAIN
-    /*
-    // CONFIRMATION EMAIL WE SEND BACK TO THE USER
+    // Confirmation email to the user (auto-reply)
     const { error: autoReplyError } = await resend.emails.send({
-      from: 'David Recuero\'s Portfolio Contact <onboarding@resend.dev>',
+      from: 'David Recuero <contact@davidrecuero.com>',
       to: data.email, 
-      subject: 'Message sent!',
-      text: `Hello ${data.name},\n\nThank you for contacting me. I have received your message and will respond as soon as possible.\n\nBest regards,\nDavid Recuero\n\n\nSubject: ${data.subject}\nMessage:\n${data.message}`,
+      subject: 'Message received!',
+      text: `Hello ${data.name},\n\nThank you for reaching out. I have received your message and will respond as soon as possible.\n\nBest regards,\nDavid Recuero\n\n---\n\nYour message:\nSubject: ${data.subject}\n\n${data.message}`,
     });
 
     // Log any error that occurs during the auto-reply process without blocking the main email sending process  
     if (autoReplyError) {
       console.error('Error with auto-reply:', autoReplyError.message);
     }
-      */
-     
+      
     return { success: true };
   } catch (error) {
     return { success: false, error: 'Internal server error' };
