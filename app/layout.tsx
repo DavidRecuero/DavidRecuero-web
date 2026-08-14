56d34f978bfc4fa904b8ef44d8972731cad384c2
David Recuero <recuero.david@gmail.com>
2026-08-14 22:45:24 +0200

    feat: vercel speed insights implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '../components/Navbar';
 import ScrollToTop from '../components/ScrollToTop';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ScrollToTop />
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
